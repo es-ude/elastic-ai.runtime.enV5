225ab9fa4f08f283512c5f5a382f99a89395b8ff
David P. Federl <david-peter.federl@stud.uni-due.de>
2022-06-02 12:22:38 +0200

    Integrade pac193x (#12)
    * added PAC193X library from student project
    
    * minor refactorings
    
    * minor changes due to compilation issue
    
    * added dummies for pac193x
    
    * updated hardware example/test for PAC193X
    
    * refactored unite tests, corrected typedefs
    
    wrong order of elements detected due to failed unit tests!
    
    * fix datatype of numberSubscriber
    
    * memory for broker domain is allocated on the heap
    
    * allow resetting broker domain during runtime
    
    * increased allocated memory for slash and null terminator
    
    * add hardware test for resetting broker domain during runtime
    
    * change receiving of uart completely
    
    * get at response
    
    * changed tcp to new uart receiving
    
    * minor refactorings for hardware tests
    
    * added PAC193X library from student project
    
    * minor refactorings
    
    * minor changes due to compilation issue
    
    * added dummies for pac193x
    
    * updated hardware example/test for PAC193X
    
    * refactored unite tests, corrected typedefs
    
    wrong order of elements detected due to failed unit tests!
    
    * fix datatype of numberSubscriber
    
    * changed tcp to new uart receiving
    
    * minor refactorings for hardware tests
    
    * revert merge error
    
    * revert merge error
    
    * resolved issues with channel indecies for rsense array
    
    Co-authored-by: David P. Federl <david-peter.federl@stud.uni-due.de>
    Co-authored-by: Maximilian Fischer <maximilian.fischer@uni-due.de>
    Co-authored-by: DerDavidos <david.lewakis@stud.uni-due.de>

---
+++ src/pac193x/pac193x_public.h
@@ -0,0 +1,75 @@
+//
+// Created by David P. Federl
+//
+
+#ifndef ENV5_PAC193X_HEADER
+#define ENV5_PAC193X_HEADER
+
+#include <stdint.h>
+#include "typedefs.h"
+
+
+/*! function that power up the sensor by setting PWRDN Pin to HIGH
+ *
+ * @return return the error code (0 if everything passed)
+ */
+pac193x_errorCode pac193x_powerUpSensor ( void );
+
+/*! function that power down the sensor by setting PWRDN Pin to LOW
+ * \Important After powered up again all settings will be set to default!
+ *
+ * @return return the error code (0 if everything passed)
+ */
+pac193x_errorCode pac193x_powerDownSensor ( void );
+
+/*! initializes the power sensor
+ *  \Important function has to be called before the sensor can be used \n
+ *             needs max 1.5ms for idle state after power up
+ *
+ * @param i2cHost[in]               i2c line to be used with the sensor
+ * @param resistanceValues[in]      array that holds the primary resistor values for each used channel
+ * @param numberOfChannelsInUse[in] number between 1 and 4 which specifies the total number of used channels
+ * @return                          return the error code (0 if everything passed)
+ */
+pac193x_errorCode pac193x_init ( i2c_inst_t * i2cHost, float resistanceValues[4], pac193x_usedChannels usedChannels );
+
+/*! updates the primary resistor values for each channel
+ *  \Important values for \b all used channels must be specified!
+ *
+ * @param resistanceValues[in] array that holds the primary resistor values used for each channel
+ * @return                     return the error code (0 if everything passed)
+ */
+pac193x_errorCode pac193x_setResistanceAtSense ( const float resistanceValues[4] );
+
+/*! updates the number of used channels
+ *
+ * @param numberOfChannelsInUse[in] number between 1 and 4 which specifies the total number of used channels
+ * @return                          return the error code (0 if everything passed)
+ */
+pac193x_errorCode pac193x_setChannelsInUse ( pac193x_usedChannels usedChannels );
+
+/*! function to retrieve the production information from the sensor
+ *
+ * @param info[out] struct that holds the information read from the sensor
+ * @return          return the error code (0 if everything passed)
+ */
+pac193x_errorCode pac193x_getSensorInfo ( pac193x_info * info );
+
+/*! function to read a specific values from the sensor for a specific channel
+ *
+ * @param channel[in]        channel where the measurement should be taken from
+ * @param valueToMeasure[in] value to be measured
+ * @param value[out]         memory where the retrieved value will be stored
+ * @return                   return the error code (0 if everything passed)
+ */
+pac193x_errorCode pac193x_getMeasurementForChannel ( pac193x_channel channel, pac193x_valueToMeasure valueToMeasure, float * value );
+
+/*! function to read \b all available values from the sensor for a specific channel
+ *
+ * @param channel[in]       channel where the measurement should be taken from
+ * @param measurements[out] memory where the struct with the measured values will be stored
+ * @return
+ */
+pac193x_errorCode pac193x_getAllMeasurementsForChannel ( pac193x_channel channel, pac193x_measurements * measurements );
+
+#endif /* ENV5_PAC193X_HEADER */
