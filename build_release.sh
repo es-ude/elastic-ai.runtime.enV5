3659f2a89836f980690331f41fc555f67920b171
David P. Federl <david-peter.federl@stud.uni-due.de>
2022-09-30 11:10:08 +0200

    updated build scripts

---
+++ build_release.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+cmake -B cmake-build-release -D UNIT_TEST:BOOL=OFF -D DEBUG_OUTPUT:BOOL=OFF -D CMAKE_BUILD_TYPE=RELEASE -G Ninja
+
+cmake --build cmake-build-release -j 4 --clean-first
