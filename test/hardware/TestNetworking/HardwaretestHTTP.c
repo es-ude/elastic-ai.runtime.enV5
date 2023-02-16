e794f769767a41f8f24c2bff02328534136a0bff
Maman, Natalie <natalie.maman@stud.uni-due.de>
2023-02-16 15:17:11 +0100

    feat(http): parse length from header, wip find out why get doesn't return correct thing

--- test/hardware/TestNetworking/HardwaretestHTTP.c
+++ test/hardware/TestNetworking/HardwaretestHTTP.c
@@ -1,37 +1,43 @@
 #define SOURCE_FILE "HTTP-TEST"
 
-#include <stdlib.h>
 #include "Common.h"
 #include "FreeRtosTaskWrapper.h"
-#include "HardwaretestHelper.h"
 #include "HTTP.h"
+#include "HardwaretestHelper.h"
 #include "string.h"
+#include <stdlib.h>
 
 void _Noreturn httpTask(void) {
     PRINT("=== STARTING TEST ===")
-    
+
     connectToNetwork();
-    for(uint8_t j=0; j<20; j++) {
-        char *response;
 
-        uint8_t code = HTTPGet("http://google.com", &response);
-
-        PRINT("returns with %d\n", code);
-        PRINT("len %d\n", strlen(response));
-
-        for (uint32_t i = 0; i < strlen(response); i++) {
-            printf("%c", response[i]);
-            if (i % 69 == 0) {
-                printf("\n");
-            }
-        }
-        printf("\n");
-        free(response);
-
-        PRINT("done\n");
-    }
-    while(1);
+    char *response;
     
+//
+//    char *baseURL = "http://192.168.0.216:5000/getfile?pos=%d";
+//    char *wholeurl = malloc(sizeof (char) * (strlen(baseURL) + 3));
+    //char *url = "http://192.168.0.216:5000/getfile";
+//    sprintf(wholeurl, baseURL, 100);
+//    PRINT_DEBUG(wholeurl);
+    uint8_t code = HTTPGet("http://192.168.0.216:5000/getfile/0", &response);
+   // uint8_t code = HTTPGet("http://httpbin.org/get", &response);
+    PRINT("returns with %d\n", code);
+    // PRINT("len %d\n", strlen(response));
+
+    for (uint32_t i = 0; i < 256; i++) {
+        printf("%c", response[i]);
+        if (i % 69 == 0) {
+            printf("\n");
+        }
+    }
+    printf("\n");
+    free(response);
+
+    PRINT("done\n");
+
+    while (1)
+        ;
 }
 
 int main() {
