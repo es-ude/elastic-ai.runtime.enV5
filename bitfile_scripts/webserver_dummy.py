388522ea7e45a9d0c022691668fd0a479c11e792
David P. Federl <david-peter.federl@stud.uni-due.de>
2023-02-24 14:12:14 +0100

    refactor(http): refactor debug webserver implementation

--- bitfile_scripts/webserver_dummy.py
+++ bitfile_scripts/webserver_dummy.py
@@ -5,29 +5,20 @@ import json
 app = Flask(__name__)
 
 
-def read_slice(position):
-    filename = 'file.txt'
-    openfile = open(filename, 'rb')
-    openfile.seek(position * 256)
-    chunk = openfile.read(100)
-    openfile.close()
+def read_slice(position: int, filename: str) -> bytes:
+    with open(filename, "rb") as file:
+        file.seek(position * 256)
+        chunk: bytes = file.read(256)
     return chunk
 
 
-# @app.route('/getfile')
-# def get_file():
-# chunk = read_slice()
-# return send_file(chunk, as_attachment=True, attachment_filename="bitfile.bit")
-
-##
-# Using positional arguments as parameter did not work
-##
-
-@app.route('/getfile/<pos>')
-def get_file(pos):
-    # position = request.args.get('pos', default = None, type = int)
+@app.route('/getfile/<position>')
+def get_file(position: str):
+    """
+    Using positional arguments as parameter did not work!
+    """
     buffer = BytesIO()
-    buffer.write(read_slice(int(pos)))
+    buffer.write(read_slice(int(position), "file.txt"))
     buffer.seek(0)
     return send_file(
         buffer,
