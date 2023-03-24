68e95c104cdb09478b52ebd4c5b1e52c3e992654
David P. Federl <david-peter.federl@stud.uni-due.de>
2023-03-24 19:16:26 +0100

    refactor(bitfile): add comments and reorder tests

--- src/env5/Env5Hw.c
+++ src/env5/Env5Hw.c
@@ -1,3 +1,6 @@
+#define SOURCE_FILE "ENV5-HW"
+
+#include "Common.h"
 #include "Env5Hw.h"
 #include "pico/stdlib.h"
 
@@ -33,6 +36,7 @@ void env5HwFpgaPowersOn() {
 
     // MOS FETS on
     gpio_put(FPGA_MOS_EN_PIN, 0);
+    PRINT_DEBUG("FPGA Powered On.")
 }
 
 void env5HwFpgaPowersOff() {
@@ -41,6 +45,7 @@ void env5HwFpgaPowersOff() {
 
     // MOS FETS on
     gpio_put(FPGA_MOS_EN_PIN, 1);
+    PRINT_DEBUG("FPGA Powered Off.")
 }
 
 void env5HwFpgaResetInit(void) {
--- test/hardware/TestNetworking/HardwaretestHttpStoreToFlash.c
+++ test/hardware/TestNetworking/HardwaretestHttpStoreToFlash.c
@@ -1,28 +1,25 @@
 #define SOURCE_FILE "HTTP-FLASH-TEST"
 
 #include "Common.h"
+#include "Flash.h"
 #include "FpgaConfigurationHttp.h"
 #include "FreeRtosTaskWrapper.h"
 #include "HTTP.h"
 #include "HardwaretestHelper.h"
-#include "string.h"
-#include <stdlib.h>
 #include "Spi.h"
-#include "Flash.h"
-#include <hardware/spi.h>
 #include "env5/Env5Hw.h"
-#include "pico/stdio.h"
 #include "middleware/middleware.h"
-#include "hardware/watchdog.h"
-#include "pico/bootrom.h"
-#include "FreeRtosQueueWrapper.h"
+#include <hardware/spi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 static const uint8_t sck_pin = 2;
 static const uint8_t miso_pin = 0;
 static const uint8_t mosi_pin = 3;
 static const uint8_t cs_pin = 1;
 static const uint32_t baudrate = 5000 * 1000;
-
+static char baseUrl[] = "http://192.168.178.24:5000/getfile/%u";
 
 void readDeviceID() {
     uint8_t id[6];
@@ -36,8 +33,21 @@ void init_helper(spi_inst_t *spi, uint32_t baudrate) {
     flashInit(cs_pin, spi);
 }
 
+HttpResponse_t *getResponse(uint32_t block_number) {
+    // todo: deep copy response and return this instead of the response pointer
 
-HttpResponse_t *getResponse(uint32_t block_number);
+    HttpResponse_t *response;
+    char *URL = malloc(strlen(baseUrl) + block_number);
+    sprintf(URL, baseUrl, block_number);
+
+    uint8_t code = HTTPGet(URL, &response);
+    PRINT_DEBUG("HTTP Get returns with %u", code);
+    PRINT_DEBUG("Response Length: %li", response->length)
+
+    free(URL);
+    PRINT("done")
+    return response;
+}
 
 void _Noreturn httpTask(void) {
     PRINT("=== STARTING TEST ===")
@@ -46,35 +56,12 @@ void _Noreturn httpTask(void) {
     env5HwInit();
 
     setCommunication(getResponse);
-    
+
     while (1) {
-    
+
         char input = getchar_timeout_us(10000);
-    
+
         switch (input) {
-        case 'i':
-            init_helper(spi, baudrate);
-            readDeviceID();
-            spiDeinit(spi, cs_pin, sck_pin, mosi_pin, miso_pin);
-            break;
-    
-        case 'F':
-            init_helper(spi, baudrate);
-            printf("ack\n");
-            configErrorCode_t configErrorCode=configure(0, 86116);
-            if(configErrorCode==CONFIG_ERASE_ERROR){
-                PRINT("ERASE ERROR")
-            }
-            PRINT("done")
-            spiDeinit(spi, cs_pin, sck_pin, mosi_pin, miso_pin);
-            break;
-        case 'V':
-            init_helper(spi, baudrate);
-            printf("ack\n");
-            fpgaConfigurationVerifyConfiguration();
-            spiDeinit(spi, cs_pin, sck_pin, mosi_pin, miso_pin);
-            break;
-    
         case 'L':
             env5HwLedsAllOn();
             break;
@@ -82,55 +69,84 @@ void _Noreturn httpTask(void) {
             env5HwLedsAllOff();
             break;
         case 'P':
+            // power on FPGA
+            // only required for old hardware before using flash
             env5HwFpgaPowersOn();
             break;
-        case 'p':
-            env5HwFpgaPowersOff();
+        case 'i':
+            // test flash accessible
+            init_helper(spi, baudrate);
+            readDeviceID();
+            spiDeinit(spi, cs_pin, sck_pin, mosi_pin, miso_pin);
+            break;
+        case 'F':
+            // load bitfile to flash
+            init_helper(spi, baudrate);
+            printf("ack\n");
+            configErrorCode_t configErrorCode = configure(0, 205272);
+            if (configErrorCode == CONFIG_ERASE_ERROR) {
+                PRINT("ERASE ERROR")
+            }
+            PRINT("done")
+            spiDeinit(spi, cs_pin, sck_pin, mosi_pin, miso_pin);
+            break;
+        case 'V':
+            // check bitfile on flash
+            init_helper(spi, baudrate);
+            printf("ack\n");
+            fpgaConfigurationVerifyConfiguration();
+            spiDeinit(spi, cs_pin, sck_pin, mosi_pin, miso_pin);
             break;
         case 'r':
-            // env5HwFpgaFlashSpiDeinit();
+            // reset FPGA
             env5HwFpgaReset(1);
             sleep_ms(10);
             env5HwFpgaReset(0);
             break;
         case 'C':
+            // load bitfile from address 0
             middleware_init();
             middleware_configure_fpga(0x0000);
             printf("reconfig 0x0000\r\n");
             middleware_deinit();
             break;
         case 'c':
+            // load bitfile from address 1
             middleware_init();
             middleware_configure_fpga(0x00100000);
             printf("reconfig to 0x00100000\r\n");
             sleep_ms(1000);
             middleware_deinit();
             break;
+        case 'p':
+            // power off FPGA
+            env5HwFpgaPowersOff();
+            break;
+
+        case 'K':
+            middleware_init();
+            middleware_set_fpga_leds(0xff);
+            uint8_t read_data = middleware_get_leds();
+            if (read_data == 0x0f)
+                printf("leds all on\r\n");
+            else
+                printf("set leds all on failed.\r\n");
+            middleware_deinit();
+            break;
+        case 'k':
+            middleware_init();
+            middleware_set_fpga_leds(0xf0);
+            if (middleware_get_leds() == 0x00)
+                printf("leds all off\r\n");
+            else
+                printf("set leds all off failed.\r\n");
+            middleware_deinit();
+            break;
+
         default:
             break;
         }
     }
-}
-
-
-HttpResponse_t *getResponse(uint32_t block_number) {
-    // todo: deep copy response and return this instead of the response pointer
-
-    HttpResponse_t *response;
-    char baseUrl[] = "http://192.168.203.51:5000/getfile/%u";
-    char *URL = malloc(strlen(baseUrl) + block_number);
-    sprintf(URL, baseUrl, block_number);
-
-    uint8_t code = HTTPGet(URL, &response);
-
-    PRINT_DEBUG("HTTP Get returns with %u", code);
-    PRINT_DEBUG("Response Length: %li", response->length)
-    //PRINT_DEBUG("Response: %s", response->response)
-
-   // HTTPCleanResponseBuffer(response);
-    free(URL);
-    PRINT("done")
-    return response;
 }
 
 int main() {
