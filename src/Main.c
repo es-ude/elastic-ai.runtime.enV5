a7c6b3c1cc3c9bf425313c048f13d0516f13aad3
David P. Federl <david-peter.federl@stud.uni-due.de>
2023-03-30 15:23:22 +0200

    style(main): introduce regions and Prototypes

--- src/Main.c
+++ src/Main.c
@@ -1,6 +1,6 @@
 #define SOURCE_FILE "MAIN"
 
-// src headers
+// internal headers
 #include "Common.h"
 #include "Esp.h"
 #include "FreeRtosQueueWrapper.h"
@@ -10,14 +10,54 @@
 #include "NetworkConfiguration.h"
 #include "Pac193x.h"
 #include "Protocol.h"
-#include "hardware/i2c.h"
 
-// external headers
+// pico-sdk headers
+#include <hardware/i2c.h>
 #include <hardware/watchdog.h>
-#include <malloc.h>
 #include <pico/bootrom.h>
 #include <pico/stdlib.h>
+
+// external headers
+#include <malloc.h>
 #include <string.h>
+
+/* region VARIABLES/DEFINES */
+
+/* region POWER-SENSOR 1 */
+
+static pac193xSensorConfiguration_t powersensor1 = {
+    .i2c_host = i2c1,
+    .i2c_slave_address = PAC193X_I2C_ADDRESS_499R,
+    .powerPin = -1,
+    .usedChannels = {.uint_channelsInUse = 0b00001111},
+    .rSense = {0.82f, 0.82f, 0.82f, 0.82f},
+};
+
+#define PAC193X_CHANNEL_SENSORS PAC193X_CHANNEL01
+#define PAC193X_CHANNEL_RAW PAC193X_CHANNEL02
+#define PAC193X_CHANNEL_MCU PAC193X_CHANNEL03
+#define PAC193X_CHANNEL_WIFI PAC193X_CHANNEL04
+
+/* endregion POWER-SENSOR 1 */
+
+/* region POWER-SENSOR 2 */
+
+static pac193xSensorConfiguration_t powersensor2 = {
+    .i2c_host = i2c1,
+    .i2c_slave_address = PAC193X_I2C_ADDRESS_806R,
+    .powerPin = -1,
+    .usedChannels = {.uint_channelsInUse = 0b00001111},
+    .rSense = {0.82f, 0.82f, 0.82f, 0.82f},
+};
+
+#define PAC193X_CHANNEL_FPGA_IO PAC193X_CHANNEL01
+#define PAC193X_CHANNEL_FPGA_1V8 PAC193X_CHANNEL02
+#define PAC193X_CHANNEL_FPGA_1V PAC193X_CHANNEL03
+#define PAC193X_CHANNEL_FPGA_SRAM PAC193X_CHANNEL04
+
+/* endregion POWER-SENSOR 2 */
+
+/* region MQTT */
 
 char *twinID;
 
@@ -30,29 +70,107 @@ typedef struct receiver {
 receiver_t receivers[5];
 uint16_t receivers_count = 0;
 
-static pac193xSensorConfiguration_t sensor1 = {
-    .i2c_host = i2c1,
-    .i2c_slave_address = PAC193X_I2C_ADDRESS_499R,
-    .powerPin = -1,
-    .usedChannels = {.uint_channelsInUse = 0b00001111},
-    .rSense = {0.82f, 0.82f, 0.82f, 0.82f},
-};
-#define PAC193X_CHANNEL_SENSORS PAC193X_CHANNEL01
-#define PAC193X_CHANNEL_RAW PAC193X_CHANNEL02
-#define PAC193X_CHANNEL_MCU PAC193X_CHANNEL03
-#define PAC193X_CHANNEL_WIFI PAC193X_CHANNEL04
+/* endregion MQTT */
 
-static pac193xSensorConfiguration_t sensor2 = {
-    .i2c_host = i2c1,
-    .i2c_slave_address = PAC193X_I2C_ADDRESS_806R,
-    .powerPin = -1,
-    .usedChannels = {.uint_channelsInUse = 0b00001111},
-    .rSense = {0.82f, 0.82f, 0.82f, 0.82f},
-};
-#define PAC193X_CHANNEL_FPGA_IO PAC193X_CHANNEL01
-#define PAC193X_CHANNEL_FPGA_1V8 PAC193X_CHANNEL02
-#define PAC193X_CHANNEL_FPGA_1V PAC193X_CHANNEL03
-#define PAC193X_CHANNEL_FPGA_SRAM PAC193X_CHANNEL04
+/* endregion VARIABLES/DEFINES */
+
+/* region PROTOTYPES */
+
+/* region HARDWARE */
+
+/// initialize hardware (watchdog, UART, ESP, WiFi, MQTT, I²C, sensors, ...)
+void init(void);
+
+/// retrieve single-shot value from power sensor
+float measureValue(pac193xSensorConfiguration_t sensor, pac193xChannel_t channel);
+
+/* endregion HARDWARE */
+
+/* region FreeRTOS TASKS */
+
+_Noreturn void fpgaTask(void);
+
+_Noreturn void sensorTask(void);
+
+/// goes into bootloader mode when 'r' is pressed
+_Noreturn void enterBootModeTask(void);
+
+/* endregion FreeRTOS TASKS */
+
+/* region MQTT */
+
+void setTwinID(char *newTwinID);
+void offline(posting_t posting);
+void offline(posting_t posting);
+void receiveDataStartRequest(posting_t posting);
+void receiveDataStopRequest(posting_t posting);
+void addDataRequestReceiver(receiver_t receiver);
+void getAndPublishSRamValue(char *dataID);
+void getAndPublishWifiValue(char *dataID);
+
+/* endregion MQTT */
+
+/* endregion HEADER */
+
+int main() {
+    init();
+
+    //    freeRtosTaskWrapperRegisterTask(fpgaTask, "fpgaTask");
+    freeRtosTaskWrapperRegisterTask(sensorTask, "sensorTask");
+    freeRtosTaskWrapperRegisterTask(enterBootModeTask, "enterBootModeTask");
+    freeRtosTaskWrapperStartScheduler();
+
+    while (true) {}
+}
+
+/* region PROTOTYPE IMPLEMENTATIONS */
+
+void init(void) {
+    // check if we crash last time -> reboot into boot rom mode
+    if (watchdog_enable_caused_reboot()) {
+        reset_usb_boot(0, 0);
+    }
+
+    // init IO
+    stdio_init_all();
+    // waits for usb connection, REMOVE to continue without waiting for connection
+    while ((!stdio_usb_connected())) {}
+    PRINT("")
+
+    // initialize ESP over UART
+    espInit();
+
+    // initialize WiFi and MQTT broker
+    networkTryToConnectToNetworkUntilSuccessful(networkCredentials);
+    mqttBrokerConnectToBrokerUntilSuccessful(mqttHost, "eip://uni-due.de/es", "enV5");
+
+    // initialize power sensors
+    pac193xErrorCode_t errorCode;
+    while (1) {
+        errorCode = pac193xInit(powersensor1);
+        if (errorCode == PAC193X_NO_ERROR) {
+            PRINT("Initialised PAC193X sensor 1.")
+            break;
+        }
+        PRINT("Initialise PAC193X failed; pac193x_ERROR: %02X\n", errorCode)
+        sleep_ms(500);
+    }
+    while (1) {
+        errorCode = pac193xInit(powersensor2);
+        if (errorCode == PAC193X_NO_ERROR) {
+            PRINT("Initialised PAC193X sensor 1.")
+            break;
+        }
+        PRINT("Initialise PAC193X failed; pac193x_ERROR: %02X\n", errorCode)
+        sleep_ms(500);
+    }
+
+    // create FreeRTOS task queue
+    freeRtosQueueWrapperCreate();
+
+    // enables watchdog timer (10s)
+    watchdog_enable(10000, 1);
+}
 
 float measureValue(pac193xSensorConfiguration_t sensor, pac193xChannel_t channel) {
     float measurement;
@@ -63,21 +181,79 @@ float measureValue(pac193xSensorConfiguration_t sensor, pac193xChannel_t channel
         PRINT("  \033[0;31mFAILED\033[0m; pac193x_ERROR: %02X", errorCode)
         return -1;
     }
+    PRINT("Measured Value: %4.2f")
     return measurement;
 }
 
+_Noreturn void fpgaTask(void) {
+    while (1) {
+        // TODO
+        freeRtosTaskWrapperTaskSleep(1000);
+    }
+}
+
+_Noreturn void sensorTask(void) {
+    addDataRequestReceiver(
+        (receiver_t){.dataID = "wifi", .whenSubscribed = getAndPublishWifiValue});
+    addDataRequestReceiver(
+        (receiver_t){.dataID = "sram", .whenSubscribed = getAndPublishSRamValue});
+
+    publishAliveStatusMessage("wifi,sram");
+
+    PRINT("Ready ...")
+
+    bool hasTwin = false;
+    while (true) {
+        bool toSomeTopicIsSubscribed = false;
+        for (int i = 0; i < receivers_count; ++i) {
+            if (receivers[i].subscribed) {
+                receivers[i].whenSubscribed(receivers[i].dataID);
+                toSomeTopicIsSubscribed = true;
+            }
+            freeRtosTaskWrapperTaskSleep(500);
+        }
+
+        if (!hasTwin && (toSomeTopicIsSubscribed)) {
+            hasTwin = true;
+            protocolSubscribeForStatus(twinID, (subscriber_t){.deliver = offline});
+        }
+        if (hasTwin && (!toSomeTopicIsSubscribed)) {
+            hasTwin = false;
+            protocolUnsubscribeFromStatus(twinID, (subscriber_t){.deliver = offline});
+        }
+    }
+}
+
+_Noreturn void enterBootModeTask(void) {
+    while (true) {
+        if (getchar_timeout_us(10) == 'r' || !stdio_usb_connected()) {
+            // enter boot mode if 'r' was received
+            reset_usb_boot(0, 0);
+        }
+
+        // watchdog update needs to be performed frequent, otherwise the device will crash
+        watchdog_update();
+        freeRtosTaskWrapperTaskSleep(1000);
+    }
+}
+
 void setTwinID(char *newTwinID) {
-    if (newTwinID == twinID)
+    if (newTwinID == twinID) {
         return;
-    if (newTwinID != NULL)
+    }
+
+    if (newTwinID != NULL) {
         free(twinID);
+    }
+
     twinID = malloc(strlen(newTwinID) + 1);
     strcpy(twinID, newTwinID);
 }
 
 void offline(posting_t posting) {
-    if (strstr(posting.data, STATUS_STATE_ONLINE) != NULL)
+    if (strstr(posting.data, STATUS_STATE_ONLINE) != NULL) {
         return;
+    }
     PRINT("Twin offline")
 
     for (int i = 0; i < receivers_count; ++i) {
@@ -109,126 +285,28 @@ void receiveDataStopRequest(posting_t posting) {
 
 void addDataRequestReceiver(receiver_t receiver) {
     receiver.subscribed = false;
+
     protocolSubscribeForDataStartRequest(receiver.dataID,
                                          (subscriber_t){.deliver = receiveDataStartRequest});
-
     protocolSubscribeForDataStopRequest(receiver.dataID,
                                         (subscriber_t){.deliver = receiveDataStopRequest});
+
     receivers[receivers_count] = receiver;
     receivers_count++;
 }
 
 void getAndPublishSRamValue(char *dataID) {
     char buffer[64];
-    float channelSensorValue = measureValue(sensor2, PAC193X_CHANNEL_FPGA_SRAM);
+    float channelSensorValue = measureValue(powersensor2, PAC193X_CHANNEL_FPGA_SRAM);
     snprintf(buffer, sizeof(buffer), "%f", channelSensorValue);
     protocolPublishData(dataID, buffer);
 }
 
 void getAndPublishWifiValue(char *dataID) {
     char buffer[64];
-    float channelWifiValue = measureValue(sensor1, PAC193X_CHANNEL_WIFI);
+    float channelWifiValue = measureValue(powersensor1, PAC193X_CHANNEL_WIFI);
     snprintf(buffer, sizeof(buffer), "%f", channelWifiValue);
     protocolPublishData(dataID, buffer);
 }
 
-_Noreturn void mainTask(void) {
-    networkTryToConnectToNetworkUntilSuccessful(networkCredentials);
-    mqttBrokerConnectToBrokerUntilSuccessful(mqttHost, "eip://uni-due.de/es", "enV5");
-
-    PRINT("===== INIT SENSOR 1 =====")
-    pac193xErrorCode_t errorCode;
-    while (1) {
-        errorCode = pac193xInit(sensor1);
-        if (errorCode == PAC193X_NO_ERROR) {
-            PRINT("Initialised PAC193X sensor 1.\n")
-            break;
-        }
-        PRINT("Initialise PAC193X failed; pac193x_ERROR: %02X\n", errorCode)
-        sleep_ms(500);
-    }
-
-    PRINT("===== INIT SENSOR 2 =====")
-    while (1) {
-        errorCode = pac193xInit(sensor2);
-        if (errorCode == PAC193X_NO_ERROR) {
-            PRINT("Initialised PAC193X sensor 1.\n")
-            break;
-        }
-        PRINT("Initialise PAC193X failed; pac193x_ERROR: %02X\n", errorCode)
-        sleep_ms(500);
-    }
-
-    sleep_ms(1000);
-
-    addDataRequestReceiver(
-        (receiver_t){.dataID = "wifi", .whenSubscribed = getAndPublishWifiValue});
-    addDataRequestReceiver(
-        (receiver_t){.dataID = "sram", .whenSubscribed = getAndPublishSRamValue});
-    
-    publishAliveStatusMessage("wifi,sram");
-
-    printf("Ready ...\n");
-
-    bool hasTwin = false;
-    while (true) {
-        bool toSomeTopicIsSubscribed = false;
-        for (int i = 0; i < receivers_count; ++i) {
-            if (receivers[i].subscribed) {
-                receivers[i].whenSubscribed(receivers[i].dataID);
-                toSomeTopicIsSubscribed = true;
-            }
-            sleep_ms(500);
-        }
-
-        if (!hasTwin && (toSomeTopicIsSubscribed)) {
-            hasTwin = true;
-            protocolSubscribeForStatus(twinID, (subscriber_t){.deliver = offline});
-        }
-        if (hasTwin && (!toSomeTopicIsSubscribed)) {
-            hasTwin = false;
-            protocolUnsubscribeFromStatus(twinID, (subscriber_t){.deliver = offline});
-        }
-    }
-}
-
-// Goes into bootloader mode when 'r' is pressed
-_Noreturn void enterBootModeTask(void) {
-    while (true) {
-        if (getchar_timeout_us(10) == 'r' || !stdio_usb_connected()) {
-            reset_usb_boot(0, 0);
-        }
-        // Watchdog update needs to be performed frequent, otherwise the device
-        // will crash
-        watchdog_update();
-        freeRtosTaskWrapperTaskSleep(1000);
-    }
-}
-
-void init(void) {
-    // First check if we crash last time -> reboot into boot rom mode
-    if (watchdog_enable_caused_reboot()) {
-        reset_usb_boot(0, 0);
-    }
-    // init usb, queue and watchdog
-    stdio_init_all();
-    // waits for usb connection, REMOVE to continue without waiting for
-    // connection
-    while ((!stdio_usb_connected()))
-        ;
-    // Checks connection to ESP and initializes
-    espInit();
-    // Create FreeRTOS task queue
-    freeRtosQueueWrapperCreate();
-    // enables watchdog to check for reboots
-    watchdog_enable(2000, 1);
-}
-
-int main() {
-    init();
-    freeRtosTaskWrapperRegisterTask(enterBootModeTask, "enterBootModeTask");
-    freeRtosTaskWrapperRegisterTask(mainTask, "mainTask");
-
-    // Starts FreeRTOS tasks
-    freeRtosTaskWrapperStartScheduler();
-}
+/* endregion PROTOTYPE IMPLEMENTATIONS */
