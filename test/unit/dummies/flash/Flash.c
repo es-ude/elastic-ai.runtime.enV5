ecd21388fc04a901e97768ee32d9e8fd26652432
Maman, Natalie <natalie.maman@stud.uni-due.de>
2023-04-06 16:21:51 +0200

    fix(HttpConfiguration): fix unittest

--- test/unit/dummies/flash/Flash.c
+++ test/unit/dummies/flash/Flash.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 uint32_t addressSectorErase = 0;
-uint32_t addressWrite[5];
+uint32_t addressWrite[12];
 uint32_t numSectorErase = 0;
 uint8_t dataComplete[FLASH_SECTOR_SIZE * 4];
 uint32_t numWriteBlocks;
@@ -23,20 +23,16 @@ uint8_t flashEraseData(uint32_t address) {
 }
 
 int flashReadData(uint32_t address, uint8_t *data_buffer, uint16_t length) {
-  //  if (address == 0) {
         for (uint16_t i = 0; i < length; i++) {
             data_buffer[i] = flash[i + address];
         }
-  //  }
+
     return 2;
 }
 int flashWritePage(uint32_t address, uint8_t *data, uint16_t page_size) {
     addressWrite[numWriteBlocks] = address;
     for (uint16_t i = 0; i < page_size; i++) {
         dataComplete[address + i] = data[i];
-        if((address+i)==1){
-            printf("%u",dataComplete[address+i]);
-        }
     }
     numWriteBlocks++;
 
