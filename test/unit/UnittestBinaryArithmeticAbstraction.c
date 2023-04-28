6b640eb1b368d32abb766304a07a5256623f1563
Jasmin Scharf <j.scharf@live.de>
2023-04-28 14:11:22 +0200

    notes for next step

--- test/unit/UnittestBinaryArithmeticAbstraction.c
+++ test/unit/UnittestBinaryArithmeticAbstraction.c
@@ -10,16 +10,18 @@ void shouldFail(void) {
 }
 
 void addShouldSucceed(void) {
-    uint8_t actual = add(1,2);
+    uint8_t actual = add(3,2);
     uint8_t expected = 3;
     TEST_ASSERT_EQUAL(expected, actual);
 }
 
+//hier schreibe ich meine Testfunktion rein, in der ich die zu testende Funktion aufrufe
+
 int main(void) {
     UNITY_BEGIN();
-
-    RUN_TEST(shouldFail);
+    
     RUN_TEST(addShouldSucceed);
+    
     
     return UNITY_END();
 }
