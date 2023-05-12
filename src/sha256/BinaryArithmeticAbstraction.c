f1aa45c180f3413b91d50ec33cd6d3c8a9ae7b36
Jasmin Scharf <j.scharf@live.de>
2023-05-12 18:05:49 +0200

    add explanaition for Maxi

--- src/sha256/BinaryArithmeticAbstraction.c
+++ src/sha256/BinaryArithmeticAbstraction.c
@@ -50,8 +50,11 @@ uint32_t bigSigmaOne(uint32_t input){  // a xor b xor c ; Σ1
 }
 
 uint32_t majority(uint32_t a, uint32_t b, uint32_t c){
-    return bitwiseXor(bitwiseXor((a + b), (a + c)), (b + c));
-} // addition > uint32_t !!!
+    return bitwiseXor(bitwiseXor((a & b), (a & c)), (b & c));
+}/* addition > uint32_t !!!
+-> https://csrc.nist.gov/csrc/media/publications/fips/180/4/final/documents/fips180-4-draft-aug2014.pdf
+page 10 (4.1.2)
+ */
 
 uint32_t choice(uint32_t e, uint32_t f, uint32_t g){
     return bitwiseXor((e & f),(bitwiseNot(e) & g));
@@ -64,6 +67,17 @@ uint32_t calculationOfWords(
     uint32_t wFourteen){
     uint32_t wSixteen = wZero + smallSigmaZero(wOne) + wNine + smallSigmaOne(wFourteen);
     return wSixteen;
-} // addition > uint32_t !!!
+} /* addition > uint32_t !!!
+ *
+ * Addition modulo 2w.
+The operation x + y is defined as follows. The words x and y represent integers X and
+Y, where 0 ≤ X < 2w and 0 ≤ Y < 2w. For positive integers U and V, let VU mod be
+the remainder upon dividing U by V. Compute
+Z=( X + Y ) mod 2w.
+Then 0 ≤ Z < 2w. Convert the integer Z to a word, z, and define z=x + y
 
-//you find explanation here   https://sha256algorithm.com/
+ https://csrc.nist.gov/csrc/media/publications/fips/180/4/final/documents/fips180-4-draft-aug2014.pdf
+*/
+
+
+//you find some other explanation here   https://sha256algorithm.com/
