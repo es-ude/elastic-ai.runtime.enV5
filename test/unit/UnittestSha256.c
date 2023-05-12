8c7278e78f68ae1dc441f8a02342900b932e9398
Jasmin Scharf <j.scharf@live.de>
2023-05-12 17:16:30 +0200

    fix UnittestSha256

--- test/unit/UnittestSha256.c
+++ test/unit/UnittestSha256.c
@@ -1,1 +1,22 @@
+#include "Sha256.h"
+#include "unity.h"
 
+void setUp(void) {}
+
+void tearDown(void) {}
+
+void shouldFail(void) {
+    TEST_FAIL();
+}
+void shouldSucceed(void) {
+    TEST_IGNORE();
+    //TEST_SUCCEED(); wirft warning
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    
+    RUN_TEST(shouldSucceed);
+    
+    return UNITY_END();
+}
