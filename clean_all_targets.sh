9c72afc3fe988dc87d3812bbb281a1ba944fdc6b
David P. Federl <david-peter.federl@stud.uni-due.de>
2023-05-24 11:06:18 +0200

    chore(unused): remove unused files/scripts

--- clean_all_targets.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-cmake --build cmake-build-test -j 4 --target clean
-
-cmake --build cmake-build-release -j 4 --target clean
-cmake --build cmake-build-debug -j 4 --target clean
-
-rm -rf out/
--- run_unittest.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-cmake -B cmake-build-test -D UNIT_TEST:BOOL=ON -D DEBUG_OUTPUT:BOOL=ON -D CMAKE_BUILD_TYPE=DEBUG -G Ninja .
-
-cmake --build cmake-build-test -j 4 --clean-first
-
-ctest --test-dir cmake-build-test/test/unit --output-on-failure
