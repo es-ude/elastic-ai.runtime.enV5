77670bba9618bf77e768b482b2d3e71ee311694f
Maximilian Fischer <boehnsins@gmail.com>
2023-05-26 11:01:39 +0200

    refactor(sha256): move functions to according file

--- src/sha256/BinaryArithmeticAbstraction.h
+++ src/sha256/BinaryArithmeticAbstraction.h
@@ -6,16 +6,13 @@
 #include "stdint.h"
 
 uint32_t rightShift(uint32_t input, uint8_t num);
+
 uint32_t leftShift(uint32_t input, uint8_t num);
+
 uint32_t rightRotate(uint32_t input, uint8_t num);
+
 uint32_t leftRotate(uint32_t input, uint8_t num);
+
 uint32_t bitwiseXor(uint32_t inputOne, uint32_t inputTwo);
+
 uint32_t bitwiseNot(uint32_t input);
-uint32_t smallSigmaZero(uint32_t input);
-uint32_t smallSigmaOne(uint32_t input);
-uint32_t bigSigmaOne(uint32_t input);
-uint32_t bigSigmaZero(uint32_t input);
-uint32_t majority(uint32_t a, uint32_t b, uint32_t c);
-uint32_t choice(uint32_t e, uint32_t f, uint32_t g);
-uint32_t additionOfWords(uint32_t a, uint32_t b);
-uint32_t calculationOfWords(uint32_t wZero, uint32_t wOne, uint32_t wNine, uint32_t wFourteen);
--- src/sha256/Sha256.h
+++ src/sha256/Sha256.h
@@ -1,4 +1,27 @@
 #ifndef ENV5_SHA256_H
 #define ENV5_SHA256_H
 
+#include "stdint.h"
+
+uint32_t smallSigmaZero(uint32_t input);
+
+uint32_t smallSigmaOne(uint32_t input);
+
+uint32_t bigSigmaOne(uint32_t input);
+
+uint32_t bigSigmaZero(uint32_t input);
+
+uint32_t majority(uint32_t a, uint32_t b, uint32_t c);
+
+uint32_t choice(uint32_t e, uint32_t f, uint32_t g);
+
+uint32_t additionOfWords(uint32_t a, uint32_t b);
+
+uint32_t calculationOfWords(
+    uint32_t wZero, 
+    uint32_t wOne, 
+    uint32_t wNine, 
+    uint32_t wFourteen
+);
+
 #endif // ENV5_SHA256_H
