99e21d06541d45df02a4b47a03d10240cc73e552
DerDavidos <david.lewakis@stud.uni-due.de>
2023-07-05 18:53:05 +0200

    fix(includeTime): on macOS, the duplicated name of the library coursed an error

--- src/time/Time.c
+++
@@ -1,6 +0,0 @@
-#include "include/Time.h"
-#include "pico/time.h"
-
-void sleep_for_ms(uint32_t msToSleep) {
-    sleep_ms(msToSleep);
-}
--- test/unit/dummies/time/Time.c
+++
@@ -1,9 +0,0 @@
-#define SOURCE_FILE "DUMMY-TIME-LIB"
-
-#include "Common.h"
-#include <stdint.h>
-
-void sleep_for_ms(uint32_t msToSleep) {
-    // Satisfy the compiler
-    PRINT("Call Sleep for %li ms", msToSleep);
-}
