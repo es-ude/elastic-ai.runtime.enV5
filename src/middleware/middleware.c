bce4f1c2a35abbb4ff08a831254e9616d1bdf0c9
Gregor Schiele <gregor.schiele@gmail.com>
2023-10-10 16:55:15 +0200

    feat: add userlogic enable and disable functions

--- src/middleware/middleware.c
+++ src/middleware/middleware.c
@@ -36,6 +36,22 @@ uint8_t middleware_get_leds(void) {
     return (0x0f & read_data[0]);
 }
 
+void middleware_userlogic_enable(void)
+{
+    uint8_t addr_arr[1];
+    addr_arr[0] = 0x00; // set user_logic_reset to 0 => enable
+
+    qxi_write_blocking(ADDR_USER_LOGIC_RESET, addr_arr, 1);
+}
+
+void middleware_userlogic_disable(void)
+{
+    uint8_t addr_arr[1];
+    addr_arr[0] = 0x01; // keep reseting the userlogic
+
+    qxi_write_blocking(ADDR_USER_LOGIC_RESET, addr_arr, 1);
+}
+
 uint8_t middleware_get_design_id(void) {
     uint8_t read_data[1];
 
