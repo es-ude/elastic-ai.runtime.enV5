a1a9e859b55c3cd09d9f41b1aca54e694c6a9337
David P. Federl <david-peter.federl@stud.uni-due.de>
2023-10-16 19:45:20 +0200

    test(midleware): update test

--- test/hardware/TestMiddleware/HardwaretestStub.c
+++ test/hardware/TestMiddleware/HardwaretestStub.c
@@ -1,43 +1,77 @@
-// This program only works with the logic design with middleware
+/* IMPORTANT: This program only works with the logic design with middleware!
+ *
+ * IMPORTANT: To reach access the wifi-network the network credentials have to be updated!
+ *
+ * NOTE: To run this test, a server that serves the HTTPGet request is required.
+ *       This server can be started by running the `HW-Test_Webserver.py` script
+ *       in the `bitfile_scripts` folder.
+ *       After starting the server, it shows an IP-address where it can be reached.
+ *       This IP address needs to be used for the `baseUrl` field.
+ *
+ * NOTE: If you update the echo_server binary file you have to update the `configSize` field
+ *       with the correct size of the file in bytes.
+ *       This size can be determined by running `du -b <path_to_file>`.
+ */
 
 #define SOURCE_FILE "HWTEST-MIDDLEWARE"
 
 #include "Common.h"
+#include "Esp.h"
 #include "Flash.h"
 #include "FpgaConfigurationHandler.h"
+#include "Network.h"
 #include "echo_server.h"
 #include "enV5HwController.h"
 #include "middleware.h"
 
+#include <hardware/spi.h>
 #include <pico/stdlib.h>
-#include "hardware/spi.h"
 
 #include <stdbool.h>
 #include <stdio.h>
+
+networkCredentials_t networkCredentials = {.ssid = "SSID", .password = "PASSWD"};
 
 spi_t spiConfiguration = {
     .spi = spi0, .baudrate = 5000000, .misoPin = 0, .mosiPin = 3, .sckPin = 2};
 uint8_t csPin = 1;
 
-static void initHardware() {
-    // enable QXI interface to the FPGA
-    middlewareInit();
+char baseUrl[] = "http://192.168.203.99:5000/getfast";
+size_t configSize = 86116;
+uint32_t configStartAddress = 0x00000000;
 
+static void initHardware() {
     // initialize the serial output
     stdio_init_all();
     while ((!stdio_usb_connected())) {
         // wait for serial connection
     }
 
+    // connect to Wi-Fi network
+    espInit();
+    networkTryToConnectToNetworkUntilSuccessful(networkCredentials);
+
+    // enable QXI interface to the FPGA
+    middlewareInit();
+
     // initialize the Flash and FPGA
     flashInit(&spiConfiguration, csPin);
     env5HwInit();
     fpgaConfigurationHandlerInitialize();
+    env5HwFpgaPowersOff();
 }
 static void loadConfigToFlash() {
-    // TODO: load config to flash
+    fpgaConfigurationHandlerError_t error = fpgaConfigurationHandlerDownloadConfigurationViaHttp(
+        baseUrl, configSize, configStartAddress);
+    if (error != FPGA_RECONFIG_NO_ERROR) {
+        while (true) {
+            PRINT("Download failed!")
+            sleep_ms(3000);
+        }
+    }
 }
 static void deployConfig() {
+    env5HwFpgaPowersOn();
     if (!echo_server_deploy()) {
         while (true) {
             PRINT("Deploy failed!")
@@ -46,16 +80,17 @@ static void deployConfig() {
     }
 }
 _Noreturn static void runTest() {
+    env5HwLedsAllOn();
     int8_t counter = 0;
     while (true) {
         uint8_t return_val = (uint8_t)echo_server_echo(counter);
 
         if (return_val == counter + 1) {
-            middlewareSetFpgaLeds(0xFF);
+            env5HwLedsAllOff();
             sleep_ms(500);
         }
 
-        middlewareSetFpgaLeds(0x00);
+        env5HwLedsAllOn();
         sleep_ms(500);
 
         counter++;
--- test/hardware/TestMiddleware/echo_server.h
+++ test/hardware/TestMiddleware/echo_server.h
@@ -1,10 +1,10 @@
-# ifndef ECHO_SERVER_STUB_H
-# define ECHO_SERVER_STUB_H
+#ifndef ECHO_SERVER_STUB_H
+#define ECHO_SERVER_STUB_H
 
-# include <stdbool.h>
-# include <stdint.h>
+#include <stdbool.h>
+#include <stdint.h>
 
 bool echo_server_deploy(void);
 int8_t echo_server_echo(int8_t inputs);
 
-# endif
+#endif
