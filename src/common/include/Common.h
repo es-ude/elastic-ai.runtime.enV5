a41dd04449671829cf088b09f48180a9090021ee
David P. Federl <david-peter.federl@stud.uni-due.de>
2023-11-26 11:28:04 +0100

    docs(common-lib): add documentation to macros

--- src/common/include/Common.h
+++ src/common/include/Common.h
@@ -7,6 +7,11 @@
 #define SOURCE_FILE "no Source file defined!"
 #endif
 
+/*!
+ * \brief print log messages
+ * \param str string to print in printf-format
+ * \param ... replacements for string
+ */
 #define PRINT(str, ...)                                                                            \
     {                                                                                              \
         printf("[%s: %s] ", SOURCE_FILE, __FUNCTION__);                                            \
@@ -14,6 +19,12 @@
         printf("\n");                                                                              \
     }
 
+/*!
+ * \brief print a byte array (uint8_t arrays)
+ * \param prefix string to print before byte array
+ * \param byteArray pointer to first byte of the array
+ * \param numberOfBytes length of the byte array
+ */
 #define PRINT_BYTE_ARRAY(prefix, byteArray, numberOfBytes)                                         \
     {                                                                                              \
         printf("[%s: %s] ", SOURCE_FILE, __FUNCTION__);                                            \
@@ -25,12 +36,24 @@
     }
 
 #ifdef DEBUG_MODE
+/*!
+ * \brief print message only in DEBUG profile
+ * \param str string to print in printf-format
+ * \param ... replacements for string
+ */
 #define PRINT_DEBUG(str, ...)                                                                      \
     {                                                                                              \
         printf("\033[0;33m[%s: %s] ", SOURCE_FILE, __FUNCTION__);                                  \
         printf(str, ##__VA_ARGS__);                                                                \
         printf("\033[0m\n");                                                                       \
     }
+
+/*!
+ * \brief print a byte array only in DEBUG profile
+ * \param prefix string to print before byte array
+ * \param byteArray pointer to first byte of the array
+ * \param numberOfBytes length of the byte array
+ */
 #define PRINT_BYTE_ARRAY_DEBUG(prefix, byteArray, numberOfBytes)                                   \
     {                                                                                              \
         printf("[%s: %s] ", SOURCE_FILE, __FUNCTION__);                                            \
@@ -46,9 +69,5 @@
 #define PRINT_BYTE_ARRAY_DEBUG(prefix, byteArray, numberOfBytes)                                   \
     {}
 #endif
-
-#define ASSERT(test)                                                                               \
-    if (!(test))                                                                                   \
-        return 0;
 
 #endif /* ENV5_COMMON_H */
