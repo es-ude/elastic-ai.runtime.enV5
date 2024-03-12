0b1f61ae3a67ee8a71d1aac984ead118d0d1338c
David P. Federl <david-peter.federl@stud.uni-due.de>
2024-03-12 11:22:53 +0100

    fix(enV5_init): update init functions

--- enV5_init.cmake
+++ enV5_init.cmake
@@ -4,14 +4,29 @@ function(enV5_init_unit_tests)
     # enable test execution via CMake
     include(CTest)
 
-    # include unity as unit-test framework
+    # include unit-test framework
     add_unity()
 
-    # add CException for libraries that use it
+    # include required standard libs for testing
     add_cexception()
+    add_runtime_c()
+    add_subdirectory(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/src/common)
+    add_subdirectory(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/network/atCommands)
+    add_subdirectory(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/network/config)
 
-    # add dummies for unit tests
+    # include dummies for local machine
     add_subdirectory(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/test/unit/dummies)
+
+    # include our own libraries to test
+    add_subdirectory(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/src/sensor/sht3x)
+    add_subdirectory(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/src/sensor/adxl345b)
+    add_subdirectory(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/src/sensor/pac193x)
+    add_subdirectory(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/src/network/wifi)
+    add_subdirectory(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/src/network/http)
+    add_subdirectory(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/src/network/broker)
+    add_subdirectory(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/src/fpga/fpgaConfigurationHandler)
+    # include our own unit-tests
+    add_subdirectory(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/test/unit)
 endfunction()
 
 function(enV5_init)
@@ -23,4 +38,7 @@ function(enV5_init)
 
     # provide access to additional sensor libraries
     add_sensor_libraries()
+
+    # include our own hardware-tests
+    add_subdirectory(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/test/hardware)
 endfunction()
