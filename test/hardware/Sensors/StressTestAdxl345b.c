b779136f680d887e1b31bcee5bdbe02f2247a0e0
David P. Federl <david-peter.federl@stud.uni-due.de>
2024-06-21 15:29:47 +0200

    fix(hardware-test): fix adxl stress test

--- test/hardware/Sensors/StressTestAdxl345b.c
+++ test/hardware/Sensors/StressTestAdxl345b.c
@@ -2,12 +2,12 @@
 
 #include "Adxl345b.h"
 #include "Common.h"
+#include "EnV5HwController.h"
 #include "Esp.h"
 #include "FreeRtosTaskWrapper.h"
 #include "HardwaretestHelper.h"
-#include "Protocol.h"
-#include "EnV5HwController.h"
 #include "I2c.h"
+#include "Protocol.h"
 
 // pico-sdk headers
 #include "hardware/i2c.h"
@@ -15,23 +15,21 @@
 #include "pico/bootrom.h"
 #include "pico/stdlib.h"
 
-
 /* endregion HELPER*/
 
 /* region I2C DEFINITION */
 i2cConfiguration_t i2cConfig = {
-        .i2cInstance = i2c1,
-        .frequency = 400000,
-        .sdaPin = 6,
-        .sclPin = 7,
+    .i2cInstance = i2c1,
+    .frequency = 400000,
+    .sdaPin = 6,
+    .sclPin = 7,
 };
 /* endregion I2C DEFINITION */
 
-
 /* region SENSOR DEFINITION */
 adxl345bSensorConfiguration_t sensor = {
-        .i2c_slave_address = ADXL345B_I2C_ALTERNATE_ADDRESS,
-        .i2c_host = i2c1,
+    .i2c_slave_address = ADXL345B_I2C_ALTERNATE_ADDRESS,
+    .i2c_host = i2c1,
 };
 /* endregion SENSOR DEFINITION */
 
@@ -67,9 +65,9 @@ void init_adxl() {
     /* initialize I2C */
     PRINT("===== START I2C INIT =====");
     i2cErrorCode_t i2cErrorCode;
-    while(1) {
+    while (1) {
         i2cErrorCode = i2cInit(&i2cConfig);
-        if (i2cErrorCode == I2C_NO_ERROR){
+        if (i2cErrorCode == I2C_NO_ERROR) {
             PRINT("Initialised I2C.");
             break;
         }
@@ -91,8 +89,8 @@ void init_adxl() {
         }
         PRINT("Initialised ADXL345B.");
 
-        errorCode =
-            adxl345bWriteConfigurationToSensor(sensor, ADXL345B_REGISTER_BW_RATE, ADXL345B_BW_RATE_800);
+        errorCode = adxl345bWriteConfigurationToSensor(sensor, ADXL345B_REGISTER_BW_RATE,
+                                                       ADXL345B_BW_RATE_800);
         if (errorCode != ADXL345B_NO_ERROR) {
             PRINT("Writing Configuration to ADXL345B failed; adxl345b_ERROR: %02X", errorCode);
             sleep_ms(500);
@@ -112,17 +110,19 @@ void init_adxl() {
 }
 
 int fibonacci(int n) {
-    if (n <= 1)
+    if (n <= 1) {
         return n;
-    else
+    } else {
         return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
 
 int factorial(int n) {
-    if (n == 0)
+    if (n == 0) {
         return 1;
-    else
+    } else {
         return n * factorial(n - 1);
+    }
 }
 
 _Noreturn void incrementingTask(void);
@@ -183,7 +183,7 @@ void _Noreturn mqttTask(void) {
     PRINT("=== STARTING TEST ===");
 
     connectToNetwork();
-    //connectToMQTT();
+    // connectToMQTT();
 
     uint64_t messageCounter = 0;
     while (1) {
@@ -205,7 +205,8 @@ _Noreturn void adxlTask(void) {
         uint32_t startTime = time_us_64();
         while (startTime + interval >= time_us_64()) {
             float xAxis, yAxis, zAxis;
-            adxl345bErrorCode_t errorCode = adxl345bReadMeasurementOneShot(sensor, & xAxis, & yAxis, & zAxis );
+            adxl345bErrorCode_t errorCode =
+                adxl345bReadMeasurementOneShot(sensor, &xAxis, &yAxis, &zAxis);
             if (errorCode == ADXL345B_NO_ERROR) {
                 /* 0.2G equals a deviation of about 1% from the ideal value
                  * this deviation is given by the datasheet as the accepted tolerance
@@ -215,7 +216,7 @@ _Noreturn void adxlTask(void) {
 
                 PRINT_DEBUG("  Expected: 01.0000G, Actual: %2.4fG = |%2.4fG| + |%2.4fG| + "
                             "|%2.4fG| = X + Y + Z",
-                                sumOfAxis, xAxis, yAxis, zAxis);
+                            sumOfAxis, xAxis, yAxis, zAxis);
                 PRINT_DEBUG(compareFloatsWithinRange(1.0f, sumOfAxis, 0.6f)
                                 ? "  \033[0;32mPASSED\033[0m"
                                 : "  \033[0;31mFAILED\033[0m");
@@ -277,8 +278,7 @@ _Noreturn void t_stressTest_enterBootModeTask(void) {
     }
 }
 
-
-//TODO: adxl_task mit MQTT_task testen
+// TODO: adxl_task mit MQTT_task testen
 
 int main(void) {
     // Did we crash last time -> reboot into boot rom mode
@@ -286,7 +286,7 @@ int main(void) {
         reset_usb_boot(0, 0);
     }
 
-    env5HwInit();
+    env5HwControllerInit();
 
     // init stdio and esp
     stdio_init_all();
@@ -296,12 +296,11 @@ int main(void) {
     //  initialize WiFi and MQTT broker
     // networkTryToConnectToNetworkUntilSuccessful(networkCredentials);
     // mqttBrokerConnectToBrokerUntilSuccessful(mqttHost, "eip://uni-due.de/es", "enV5");
-    
-    
-    //vor der init schlafen lassen um sicher zu stellen, dass pico nicht zu schnell ist und adxl zu langsam nach reboot
+
+    // vor der init schlafen lassen um sicher zu stellen, dass pico nicht zu schnell ist und adxl zu
+    // langsam nach reboot
     sleep_ms(1000);
     init_adxl();
-    
 
     while (1) {
         char input = getchar_timeout_us(10000000); /* 10 seconds wait */
@@ -327,15 +326,16 @@ int main(void) {
             t_stressTest_hard();
             freeRtosTaskWrapperStartScheduler();
             break;
-            case 'q':
+        case 'q':
             t_stressTest_withMQTT();
             break;
-            
+
         case 'b':
             enterBootMode();
             break;
         default:
-            PRINT("Please enter to request for intensity:\n o (only adxl),\n f (freeRTOS with adxl),\n e (easy),\n m (mediocre),\n h "
+            PRINT("Please enter to request for intensity:\n o (only adxl),\n f (freeRTOS with "
+                  "adxl),\n e (easy),\n m (mediocre),\n h "
                   "(hard),\n q (adxl with mqtt),\n b (Boot mode)");
             break;
         }
