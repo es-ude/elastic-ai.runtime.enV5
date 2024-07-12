05f5b32390ff5987cc29f89ccb0086a531cae6e4
PandiasChild <89422757+PandiasChild@users.noreply.github.com>
2024-07-12 16:25:13 +0200

    fix(test): update adxl tests according to changes

--- test/unit/UnittestAdxl345b.c
+++ test/unit/UnittestAdxl345b.c
@@ -75,45 +75,56 @@ void adxl345bReadSerialNumberReadCorrectValue(void) {
 /* region adxl345bReadMeasurementOneShot */
 
 void adxl345bReadMeasurementsGetSendCommandFail_errorIfHardwarFails(void) {
-    float xAxis = 0, yAxis = 0, zAxis = 0;
+    uint8_t rawDataSizeSix[6];
     i2cUnittestWriteCommand = i2cUnittestWriteCommandHardwareDefect;
 
-    adxl345bErrorCode_t errorCode = adxl345bReadMeasurementOneShot(sensor, &xAxis, &yAxis, &zAxis);
+    adxl345bErrorCode_t errorCode = adxl345bReadMeasurementOneShot(sensor, rawDataSizeSix);
     TEST_ASSERT_EQUAL_UINT8(ADXL345B_SEND_COMMAND_ERROR, errorCode);
 }
 
 void adxl345bReadMeasurementsGetSendCommandFail_errorIfAckMissing(void) {
-    float xAxis = 0, yAxis = 0, zAxis = 0;
+    uint8_t rawDataSizeSix[6];
     i2cUnittestWriteCommand = i2cUnittestWriteCommandAckMissing;
 
-    adxl345bErrorCode_t errorCode = adxl345bReadMeasurementOneShot(sensor, &xAxis, &yAxis, &zAxis);
+    adxl345bErrorCode_t errorCode = adxl345bReadMeasurementOneShot(sensor, rawDataSizeSix);
     TEST_ASSERT_EQUAL_UINT8(ADXL345B_SEND_COMMAND_ERROR, errorCode);
 }
 
 void adxl345bReadMeasurementsGetReceiveDataFail_errorIfHardwarFails(void) {
-    float xAxis = 0, yAxis = 0, zAxis = 0;
+    uint8_t rawDataSizeSix[6];
     i2cUnittestReadCommand = i2cUnittestReadCommandHardwareDefect;
 
-    adxl345bErrorCode_t errorCode = adxl345bReadMeasurementOneShot(sensor, &xAxis, &yAxis, &zAxis);
+    adxl345bErrorCode_t errorCode = adxl345bReadMeasurementOneShot(sensor, rawDataSizeSix);
     TEST_ASSERT_EQUAL_UINT8(ADXL345B_RECEIVE_DATA_ERROR, errorCode);
 }
 
 void adxl345bReadMeasurementsGetReceiveDataFail_errorIfAckMissing(void) {
-    float xAxis = 0, yAxis = 0, zAxis = 0;
+    uint8_t rawDataSizeSix[6];
     i2cUnittestReadCommand = i2cUnittestReadCommandAckMissing;
 
-    adxl345bErrorCode_t errorCode = adxl345bReadMeasurementOneShot(sensor, &xAxis, &yAxis, &zAxis);
+    adxl345bErrorCode_t errorCode = adxl345bReadMeasurementOneShot(sensor, rawDataSizeSix);
     TEST_ASSERT_EQUAL_UINT8(ADXL345B_RECEIVE_DATA_ERROR, errorCode);
 }
 
 void adxl345bReadMeasurementsReadSuccessful(void) {
-    float xAxis = 0, yAxis = 0, zAxis = 0;
+    uint8_t rawDataSizeSix[6];
 
-    uint8_t err = adxl345bReadMeasurementOneShot(sensor, &xAxis, &yAxis, &zAxis);
+    uint8_t err = adxl345bReadMeasurementOneShot(sensor, rawDataSizeSix);
     TEST_ASSERT_EQUAL_UINT8(ADXL345B_NO_ERROR, err);
 }
 
 void adxl345bReadMeasurementsReadCorrectValue(void) {
+    uint8_t sizeOfRawData = 6;
+    uint8_t rawDataSizeSix[sizeOfRawData];
+
+    adxl345bReadMeasurementOneShot(sensor, rawDataSizeSix);
+    for(int i = 0; i < sizeOfRawData; i ++){
+        TEST_ASSERT_EQUAL_UINT8(byteZero, rawDataSizeSix[i]);
+    }
+
+}
+
+void adxl345bConvertDataXYZCorrectValue(void) {
     /* test assumes that 2G Full Range is the used Range */
 
     float expected_xAxis = 0, expected_yAxis = 0, expected_zAxis = 0;
@@ -122,6 +133,13 @@ void adxl345bReadMeasurementsReadCorrectValue(void) {
     const float SCALE_FACTOR_FOR_RANGE = 0.0043f;
     /* only used lower 2 bits -> 2G Range consists of 10 Bit*/
     uint8_t topByte = byteZero & MSB_MASK;
+
+    /* set rawData */
+    uint8_t sizeOfRawData = 6;
+    uint8_t rawDataSizeSix[sizeOfRawData];
+    for(int i = 0; i < sizeOfRawData; i ++){
+            rawDataSizeSix[i] = byteZero;
+    }
 
     /* fill expected with random generated */
     if (topByte <= (MSB_MASK >> 1)) {
@@ -144,7 +162,7 @@ void adxl345bReadMeasurementsReadCorrectValue(void) {
         expected_xAxis = expected_yAxis = expected_zAxis = realValue;
     }
 
-    adxl345bReadMeasurementOneShot(sensor, &actual_xAxis, &actual_yAxis, &actual_zAxis);
+    adxl345bConvertDataXYZ(&actual_xAxis, &actual_yAxis, &actual_zAxis, rawDataSizeSix);
     TEST_ASSERT_EQUAL_FLOAT(expected_xAxis, actual_xAxis);
     TEST_ASSERT_EQUAL_FLOAT(expected_yAxis, actual_yAxis);
     TEST_ASSERT_EQUAL_FLOAT(expected_zAxis, actual_zAxis);
@@ -168,6 +186,8 @@ int main(void) {
     RUN_TEST(adxl345bReadMeasurementsGetReceiveDataFail_errorIfAckMissing);
     RUN_TEST(adxl345bReadMeasurementsReadSuccessful);
     RUN_TEST(adxl345bReadMeasurementsReadCorrectValue);
+
+    RUN_TEST(adxl345bConvertDataXYZCorrectValue);
 
     return UNITY_END();
 }
