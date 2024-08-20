e1f0e01f1891593e4c4db773f8f805cf383c07fb
Leo Buron <leoburon10@gmail.com>
2024-08-20 20:28:19 +0200

    feat: added functions to detect raspberry pi devices. In the future we might want to change the pid and vid to our gotten one

---
+++ python_utils/usb/helper_utils.py
@@ -0,0 +1,16 @@
+from serial.tools import list_ports
+
+
+def get_env5_port() -> str:
+    for ports in list_ports.comports():
+        if ports.vid == 11914:
+            if ports.pid == 10 or ports.vid == 3:
+                return ports.device
+
+
+def is_env5_in_bootselect_stage() -> bool:
+    for ports in list_ports.comports():
+        if ports.vid == 11914:
+            if ports.vid == 3:
+                return True
+
