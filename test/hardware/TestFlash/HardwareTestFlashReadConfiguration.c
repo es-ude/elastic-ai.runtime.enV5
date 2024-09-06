61f2ae5ea5b50e28e38baebab6cbe947fbe478f4
Jan <jan.koch@stud.ui-due.de>
2024-09-06 09:05:15 +0200

    fix(flash): change further flashConfig parameters to spiConfig

--- test/hardware/TestFlash/HardwareTestFlashReadConfiguration.c
+++ test/hardware/TestFlash/HardwareTestFlashReadConfiguration.c
@@ -47,7 +47,7 @@ void initializeHardware(void) {
 
 void enableQuadSPI(void) {
     uint8_t config[] = {0x00, 0x02};
-    flashWriteConfig(&flashConfig, config, sizeof(config));
+    flashWriteConfig(&spiToFlashConfig, config, sizeof(config));
 }
 
 void readConfig(uint8_t registerToRead) {
