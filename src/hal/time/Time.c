d967809c4296d1cafedb736dec08ae9092192b04
PandiasChild <89422757+PandiasChild@users.noreply.github.com>
2024-09-20 14:50:59 +0200

    refactor(hal): rename functions of time_interface lib
    BREAKING CHANGE: change names of time_library functions get_time_in...

--- src/hal/time/Time.c
+++ src/hal/time/Time.c
@@ -3,11 +3,11 @@
 #include "pico/time.h"
 #include <stdint.h>
 
-uint64_t get_current_time_in_us(void) {
+uint64_t timeUs64(void) {
     return (uint64_t)get_absolute_time();
 }
 
-uint32_t get_current_time_in_ms(void) {
+uint32_t timeMs32(void) {
     uint64_t time = (uint64_t)get_absolute_time();
     return us_to_ms(time);
 }
