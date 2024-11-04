61815be0391e118cabe39bbba8ef1736cedfb6cd
Lukas Einhaus <lukas.einhaus@uni-due.de>
2024-11-04 11:59:45 +0100

    fix(cmake): add missing cmake targets

--- cmake/pico_targets.cmake
+++ cmake/pico_targets.cmake
@@ -8,19 +8,34 @@ function(__add_pico_lib original)
         add_library(Pico::${original}__hdrs ALIAS pico_empty_lib)
         add_library(Pico::${original} ALIAS pico_empty_lib)
         add_library(Pico::${original}__impl ALIAS pico_empty_lib)
+        add_library(Pico::${original}__impl_with_deps ALIAS pico_empty_lib)
     else ()
         add_library(Pico::${original} ALIAS ${original})
         add_library(Pico::${original}__hdrs ALIAS ${original}_headers)
         add_library(Pico::${original}__impl ALIAS ${original})
+        add_library(Pico::${original}__impl_with_deps ALIAS ${original})
     endif ()
 endfunction()
 
 
 foreach (arg
-        pico_runtime hardware_gpio pico_stdlib pico_stdio_usb pico_bootrom
-        hardware_i2c hardware_spi pico_util pico_platform pico_clib_interface
-        pico_time hardware_timer hardware_rtc
-        hardware_uart hardware_irq
+        pico_runtime
+        pico_util
+        pico_platform
+        pico_clib_interface
+        pico_time
+        pico_stdlib
+        pico_stdio
+        pico_bootrom
+        pico_stdio_usb
+        pico_sync
+        hardware_gpio
+        hardware_i2c
+        hardware_spi
+        hardware_timer
+        hardware_rtc
+        hardware_uart
+        hardware_irq
 )
     __add_pico_lib(${arg})
 endforeach ()
