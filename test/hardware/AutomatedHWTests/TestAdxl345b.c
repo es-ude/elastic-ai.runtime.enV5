77b5772bc47646b1dd52e6ac40bc5f4e24b90b3f
PandiasChild <89422757+PandiasChild@users.noreply.github.com>
2025-04-24 14:03:59 +0200

    feat(hardware-test): setup and init automated ADXL test
    # Conflicts:
    #	test/hardware/AutomatedHWTests/CMakeLists.txt

---
+++ test/hardware/AutomatedHWTests/TestAdxl345b.c
@@ -0,0 +1,89 @@
+#define SOURCE_FILE "ADXL345B-HWTEST"
+
+#include "eai/Common.h"
+#include "eai/hal/EnV5HwConfiguration.h"
+#include "eai/hal/I2c.h"
+#include "eai/sensor/Adxl345b.h"
+
+#include "hardware/i2c.h"
+#include "unity.h"
+
+#include <pico/bootrom.h>
+#include <pico/stdio_usb.h>
+#include <pico/time.h>
+
+#define paramTest(fn, param)                                                                       \
+    void fn##param(void) {                                                                         \
+        fn(param);                                                                                 \
+    }
+
+/* region HELPER */
+
+static uint16_t samplesForTrigger = 30;
+
+/* endregion HELPER*/
+
+/* region I2C DEFINITION */
+i2cConfiguration_t i2cConfig = {
+    .i2cInstance = I2C_MODULE,
+    .frequency = I2C_FREQUENCY_IN_HZ,
+    .sdaPin = I2C_SDA_PIN,
+    .sclPin = I2C_SCL_PIN,
+};
+
+/* endregion I2C DEFINITION */
+
+/* region SENSOR DEFINITION */
+adxl345bSensorConfiguration_t sensor = {
+    .i2c_slave_address = ADXL_SLAVE,
+    .i2c_host = ADXL_I2C_MODULE,
+};
+/* endregion SENSOR DEFINITION */
+
+void init() {
+    /* enable print to console */
+    stdio_init_all();
+    /* wait for user console to connect */
+    while (!stdio_usb_connected()) {}
+    sleep_ms(500);
+
+    /* initialize I2C */
+    i2cErrorCode_t i2cErrorCode;
+    while (1) {
+        i2cErrorCode = i2cInit(&i2cConfig);
+        if (i2cErrorCode == I2C_NO_ERROR) {
+            break;
+        }
+        PRINT("Initialise I2C failed; i2c_ERROR: %02X", i2cErrorCode);
+        sleep_ms(500);
+    }
+
+    /* initialize ADXL345B sensor */
+    adxl345bErrorCode_t errorCode;
+    while (1) {
+        errorCode = adxl345bInit(sensor);
+        if (errorCode == ADXL345B_NO_ERROR) {
+            break;
+        }
+        PRINT("Initialise ADXL345B failed; adxl345b_ERROR: %02X", errorCode);
+        sleep_ms(500);
+    }
+}
+
+void setUp() {}
+void tearDown() {};
+
+void deInit() {
+    rom_reset_usb_boot(0, 0);
+}
+
+int main() {
+    init();
+    UNITY_BEGIN();
+    /* do some tests */
+    UNITY_END();
+    deInit();
+
+    return 0;
+}
+
