a99d5bc7c1a5dd146f74956172b310890a537e0d
Leo Buron <leo.buron@uni-due.de>
2025-08-08 13:25:06 +0200

    feat(OnDeviceTraining): add MSEDOuput

--- src/ai/loss_functions/MSE.c
+++ src/ai/loss_functions/MSE.c
@@ -3,5 +3,9 @@
 #include "MSE.h"
 
 float *MSELossDOutput(float *output, float *label, size_t size) {
-    ;
+    float *result = calloc(size, sizeof(float));
+    for (size_t i = 0; i < size; i++) {
+        result[i] = output[i] - label[i];
+    }
+    return result;
 }
