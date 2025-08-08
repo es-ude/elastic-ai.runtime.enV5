88255df98cd71332bcb2c4aee68249d11dbca5da
Jan Koch <jan.koch@stud.uni-due.de>
2025-08-08 14:19:15 +0200

    feat(OnDeviceTraining): implement ReLU

--- test/unit/ai/nn/UnitTestReLU.c
+++ test/unit/ai/nn/UnitTestReLU.c
@@ -2,19 +2,18 @@
 
 #include <stdlib.h>
 
-#include "unity.h"
 #include "ReLU.h"
+#include "unity.h"
 
 void unitTestReLUForward() {
     float input[] = {-1.f, 0.f, 1.f, 2.f, 5.f, -6.f};
 
-    ReLUConfig_t config;
-    config.size = sizeof(input)/sizeof(float);
+    ReLUConfig_t *config = initReLUConfig(sizeof(input) / sizeof(float));
 
-    float *result = ReLUForward(&config, input);
+    float *result = ReLUForward(config, input);
     float expected[] = {0.f, 0.f, 1.f, 2.f, 5.f, 0.f};
 
-    TEST_ASSERT_EQUAL_FLOAT_ARRAY(expected, result, config.size);
+    TEST_ASSERT_EQUAL_FLOAT_ARRAY(expected, result, config->size);
 }
 
 void unitTestReLUBackward() {
@@ -22,13 +21,16 @@ void unitTestReLUBackward() {
     float grad[] = {0.f, 2.f, -4.f, 6.f, 3.f, 2.f};
 
     ReLUConfig_t config;
-    config.size = sizeof(input)/sizeof(float);
+    config.size = sizeof(input) / sizeof(float);
 
     float *result = ReLUBackward(&config, grad, input);
     float expected[] = {0.f, 0.f, -4.f, 6.f, 3.f, 0.f};
 
     TEST_ASSERT_EQUAL_FLOAT_ARRAY(expected, result, config.size);
 }
+
+void setUp() {}
+void tearDown() {}
 
 int main() {
     UNITY_BEGIN();
