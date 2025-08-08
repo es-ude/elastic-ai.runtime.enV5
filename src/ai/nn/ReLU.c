0f0d7a806842f534ce056fd09b141f4362fb0066
Jan Koch <jan.koch@stud.uni-due.de>
2025-08-08 14:19:15 +0200

    feat(OnDeviceTraining): implement ReLU

--- src/ai/nn/ReLU.c
+++ src/ai/nn/ReLU.c
@@ -3,12 +3,34 @@
 #include "ReLU.h"
 
 ReLUConfig_t *initReLUConfig(size_t size) {
-    ;
+    ReLUConfig_t *config = calloc(1, sizeof(ReLUConfig_t));
+    config->size = size;
+    return config;
 }
 
 float *ReLUForward(ReLUConfig_t *config, float *input) {
-    ;
+
+    float *output = calloc(config->size, sizeof(float));
+    for (size_t index = 0; index < config->size; index++) {
+        if (input[index] < 0.0f) {
+            output[index] = 0.f;
+        } else {
+            output[index] = input[index];
+        }
+    }
+    return output;
 }
 float *ReLUBackward(ReLUConfig_t *config, float *grad, float *input) {
-    ;
+    float *outputGrad = calloc(config->size, sizeof(float));
+
+    for (size_t i = 0; i < config->size; i++) {
+        if (input[i] <= 0.0f) {
+            outputGrad[i] = 0.0f;
+        } else {
+            outputGrad[i] = grad[i];
+        }
+    }
+    return outputGrad;
 }
+
+
