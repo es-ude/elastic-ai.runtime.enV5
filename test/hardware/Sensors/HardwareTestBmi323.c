0ee6f1cfadd5fa3da9262d05a3a17745688b1185
PandiasChild <89422757+PandiasChild@users.noreply.github.com>
2025-09-29 19:45:43 +0200

    This is the 1st commit message:
    feat(sensor): remove CException from bmi323
    
    BREAKING CHANGE: add errorCodes instead of C-Exception
    t Please enter the commit message for your changes. Lines starting

--- test/hardware/Sensors/HardwareTestBmi323.c
+++ test/hardware/Sensors/HardwareTestBmi323.c
@@ -5,8 +5,6 @@
 #include "pico/stdio.h"
 #include "pico/stdio_usb.h"
 
-#include "CException.h"
-
 #include "eai/Common.h"
 #include "eai/hal/EnV5HwConfiguration.h"
 #include "eai/hal/EnV5HwController.h"
@@ -44,74 +42,100 @@ static void gotoBootloader(void) {
 }
 
 static void initializeBmi323(void) {
+    bmi323ErrorCode_t errorCode;
     spiInit(&spi);
-    PRINT_DEBUG("Initialized SPI");
+    PRINT_DEBUG("SPI initialized ");
 
-    CEXCEPTION_T exception;
-    Try {
-        bmi323Init(bmi323, &spi);
-    }
-    Catch(exception) {
-        PRINT("ERROR: %i", exception);
-        gotoBootloader();
+    while (1) {
+        errorCode = bmi323Init(bmi323, &spi);
+        if (errorCode != BMI323_OK) {
+            PRINT("\033Initialise bmi323 failed\033; bmi323_ERROR: %02X", errorCode);
+            sleep_ms(500);
+            continue;
+        }
     }
     PRINT("BMI323 initialized");
 }
 
 static void getChipId(void) {
-    CEXCEPTION_T exception;
-    Try {
+    bmi323ErrorCode_t errorCode;
         uint8_t chipId[2];
         data_t idData = {.data = chipId, .length = sizeof(chipId)};
-        bmi323GetRegister(bmi323, BMI3_REG_CHIP_ID, &idData);
+        errorCode = bmi323GetRegister(bmi323, BMI3_REG_CHIP_ID, &idData);
+        if(errorCode != BMI323_OK){
+            PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+            return;
+        }
         PRINT("Chip-ID: 0x%04X", chipId[0] << 8 | chipId[1]);
-    }
-    Catch(exception) {
-        PRINT("ERROR: %i", exception);
-        gotoBootloader();
-    }
+        return;
 }
 
 static void initializeAccelerometer(void) {
-    CEXCEPTION_T exception;
-    Try {
+    bmi323ErrorCode_t errorCode;
         bmi323InterruptMapping_t mapping = {.acc_drdy_int = BMI323_INTERRUPT_1};
         bmi323SetInterruptMapping(bmi323, mapping);
+        if(errorCode != BMI323_OK){
+            PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+            return;
+        }
         PRINT("Interrupt Mapped");
 
         bmi323FeatureConfiguration_t config[1];
         config[0].type = BMI323_ACCEL;
         bmi323GetSensorConfiguration(bmi323, 1, config);
+        if(errorCode != BMI323_OK){
+            PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+            return;
+        }
         config[0].cfg.acc.odr = BMI3_ACC_ODR_100HZ;
         config[0].cfg.acc.range = BMI3_ACC_RANGE_16G;
         config[0].cfg.gyr.bwp = BMI3_ACC_BW_ODR_QUARTER;
         config[0].cfg.gyr.avg_num = BMI3_ACC_AVG1;
         config[0].cfg.gyr.gyr_mode = BMI3_ACC_MODE_NORMAL;
         bmi323SetSensorConfiguration(bmi323, 1, config);
+        if(errorCode != BMI323_OK){
+            PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+            return;
+        }
         PRINT("Accelerometer configured");
-    }
-    Catch(exception) {
-        PRINT("Error: %i", exception);
-        gotoBootloader();
-    }
 }
 static void getAccelerometerData() {
-    CEXCEPTION_T exception;
+    bmi323ErrorCode_t errorCode;
+    uint16_t interrupt;
     uint8_t index = 0;
     while (index <= LIMIT) {
-        Try {
-            uint16_t interrupt = bmi323GetInterruptStatus(bmi323, BMI323_INTERRUPT_1);
+            errorCode = bmi323GetInterruptStatus(bmi323, BMI323_INTERRUPT_1, &interrupt);
+            if(errorCode != BMI323_OK){
+                PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+                return;
+            }
             if (interrupt & BMI3_INT_STATUS_ACC_DRDY) {
                 bmi323SensorData_t data[1];
                 data[0].type = BMI323_ACCEL;
                 bmi323GetData(bmi323, 1, data);
-
-                float x = bmi323LsbToMps2(data[0].sens_data.acc.x, BMI3_ACC_RANGE_16G,
-                                          bmi323[0].resolution);
-                float y = bmi323LsbToMps2(data[0].sens_data.acc.y, BMI3_ACC_RANGE_16G,
-                                          bmi323[0].resolution);
-                float z = bmi323LsbToMps2(data[0].sens_data.acc.z, BMI3_ACC_RANGE_16G,
-                                          bmi323[0].resolution);
+                if(errorCode != BMI323_OK){
+                    PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+                    return;
+                }
+                float x, y, z;
+                errorCode = bmi323LsbToMps2(data[0].sens_data.acc.x, BMI3_ACC_RANGE_16G,
+                                          bmi323[0].resolution, &x);
+                if(errorCode != BMI323_OK){
+                    PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+                    return;
+                }
+                errorCode = bmi323LsbToMps2(data[0].sens_data.acc.y, BMI3_ACC_RANGE_16G,
+                                          bmi323[0].resolution, &y);
+                if(errorCode != BMI323_OK){
+                    PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+                    return;
+                }
+                errorCode = bmi323LsbToMps2(data[0].sens_data.acc.z, BMI3_ACC_RANGE_16G,
+                                          bmi323[0].resolution, &z);
+                if(errorCode != BMI323_OK){
+                    PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+                    return;
+                }
 
                 PRINT("%d:\n"
                       "\tRange=%dg\n"
@@ -122,10 +146,6 @@ static void getAccelerometerData() {
 
                 index++;
             }
-        }
-        Catch(exception) {
-            PRINT("ERROR: %i", exception);
-        }
     }
 }
 static void testAccelerometer(void) {
@@ -134,53 +154,83 @@ static void testAccelerometer(void) {
 }
 
 static void initializeGyroscope(void) {
-    CEXCEPTION_T exception;
-    Try {
+    bmi323ErrorCode_t errorCode;
         bmi323FeatureConfiguration_t config[0];
         config[0].type = BMI323_GYRO;
-        bmi323GetSensorConfiguration(bmi323, 1, config);
+        errorCode = bmi323GetSensorConfiguration(bmi323, 1, config);
+        if(errorCode != BMI323_OK){
+            PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+            return;
+        }
         config[0].cfg.gyr.odr = BMI3_GYR_ODR_100HZ;
         config[0].cfg.gyr.range = BMI3_GYR_RANGE_2000DPS;
         config[0].cfg.gyr.bwp = BMI3_GYR_BW_ODR_HALF;
         config[0].cfg.gyr.gyr_mode = BMI3_GYR_MODE_NORMAL;
         config[0].cfg.gyr.avg_num = BMI3_GYR_AVG1;
-        bmi323SetSensorConfiguration(bmi323, 1, config);
+        errorCode = bmi323SetSensorConfiguration(bmi323, 1, config);
+        if(errorCode != BMI323_OK){
+            PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+            return;
+        }
         PRINT("Gyroscope configured");
 
         bmi323InterruptMapping_t mapping = {.gyr_drdy_int = BMI323_INTERRUPT_1};
-        bmi323SetInterruptMapping(bmi323, mapping);
+        errorCode = bmi323SetInterruptMapping(bmi323, mapping);
+        if(errorCode != BMI323_OK){
+            PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+            return;
+        }
         PRINT("Interrupt Mapped");
 
         bmi323SelfCalibrationResults_t calibration;
-        bmi323PerformGyroscopeSelfCalibration(bmi323, BMI3_SC_OFFSET_EN, BMI3_SC_APPLY_CORR_EN,
+        errorCode = bmi323PerformGyroscopeSelfCalibration(bmi323, BMI3_SC_OFFSET_EN, BMI3_SC_APPLY_CORR_EN,
                                               &calibration);
+        if(errorCode != BMI323_OK){
+            PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+            return;
+        }
         PRINT("Calibration Results: %X", calibration.gyro_sc_rslt);
-    }
-    Catch(exception) {
-        PRINT("Error: %i", exception);
-        gotoBootloader();
-    }
 }
 static void getGyroscopeData() {
-    CEXCEPTION_T exception;
+    bmi323ErrorCode_t errorCode;
+    uint16_t interrupt;
     uint8_t index = 0;
     while (index <= LIMIT) {
-        Try {
-            uint16_t interrupt = bmi323GetInterruptStatus(bmi323, BMI323_INTERRUPT_1);
+            errorCode = bmi323GetInterruptStatus(bmi323, BMI323_INTERRUPT_1, interrupt);
+            if(errorCode != BMI323_OK){
+                PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+                return;
+            }
             if (!(interrupt & BMI3_INT_STATUS_GYR_DRDY)) {
                 continue;
             }
 
             bmi323SensorData_t data[1];
             data[0].type = BMI323_GYRO;
-            bmi323GetData(bmi323, 1, data);
-
-            float x = bmi323LsbToDps(data[0].sens_data.gyr.x, BMI3_GYR_RANGE_2000DPS,
+            errorCode = bmi323GetData(bmi323, 1, data);
+            if(errorCode != BMI323_OK){
+                PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+                return;
+            }
+            float x,y,z;
+            errorCode = bmi323LsbToDps(data[0].sens_data.gyr.x, BMI3_GYR_RANGE_2000DPS,
                                      bmi323[0].resolution);
-            float y = bmi323LsbToDps(data[0].sens_data.gyr.y, BMI3_GYR_RANGE_2000DPS,
+            if(errorCode != BMI323_OK){
+                PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+                return;
+            }
+            errorCode = bmi323LsbToDps(data[0].sens_data.gyr.y, BMI3_GYR_RANGE_2000DPS,
                                      bmi323[0].resolution);
-            float z = bmi323LsbToDps(data[0].sens_data.gyr.z, BMI3_GYR_RANGE_2000DPS,
+            if(errorCode != BMI323_OK){
+                PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+                return;
+            }
+            errorCode = bmi323LsbToDps(data[0].sens_data.gyr.z, BMI3_GYR_RANGE_2000DPS,
                                      bmi323[0].resolution);
+            if(errorCode != BMI323_OK){
+                PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+                return;
+            }
 
             PRINT("%d:\n"
                   "\tRange=%dDPS\n"
@@ -190,10 +240,6 @@ static void getGyroscopeData() {
                   data[0].sens_data.gyr.z, x, y, z);
 
             index++;
-        }
-        Catch(exception) {
-            PRINT("ERROR: %i", exception);
-        }
     }
 }
 static void testGyroscope(void) {
@@ -202,42 +248,53 @@ static void testGyroscope(void) {
 }
 
 static void initializeTemperature(void) {
-    CEXCEPTION_T exception;
-    Try {
+    bmi323ErrorCode_t errorCode;
         bmi323InterruptMapping_t mapping = {.temp_drdy_int = BMI323_INTERRUPT_1};
-        bmi323SetInterruptMapping(bmi323, mapping);
+        errorCode = bmi323SetInterruptMapping(bmi323, mapping);
+        if(errorCode != BMI323_OK){
+            PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+            return;
+        }
         PRINT("Interrupt Enabled");
 
         bmi323FeatureConfiguration_t config[1];
         config[0].type = BMI3_ACCEL;
-        bmi323GetSensorConfiguration(bmi323, 1, config);
+        errorCode = bmi323GetSensorConfiguration(bmi323, 1, config);
+        if(errorCode != BMI323_OK){
+            PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+            return;
+        }
         config[0].cfg.acc.acc_mode = BMI3_ACC_MODE_NORMAL;
-        bmi323SetSensorConfiguration(bmi323, 1, config);
+        errorCode = bmi323SetSensorConfiguration(bmi323, 1, config);
+        if(errorCode != BMI323_OK){
+            PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+            return;
+        }
         PRINT("Sensor Configured");
-    }
-    Catch(exception) {
-        PRINT("ERROR: %i", exception);
-        gotoBootloader();
-    }
 }
 static void getTemperatureData() {
+    bmi323ErrorCode_t errorCode;
+    uint16_t interruptStatus;
+    float *temperature;
+    float *sensorTime;
     uint8_t index = 0;
     while (index <= LIMIT) {
-        CEXCEPTION_T exception;
-        Try {
-            uint16_t interruptStatus = bmi323GetInterruptStatus(bmi323, BMI323_INTERRUPT_1);
+            errorCode = bmi323GetInterruptStatus(bmi323, BMI323_INTERRUPT_1, interruptStatus);
             if (interruptStatus & BMI3_INT_STATUS_TEMP_DRDY) {
-                float temperature = bmi323GetTemperature(bmi323);
-                float sensorTime = bmi323GetSensorTime(bmi323);
+                errorCode = bmi323GetTemperature(bmi323, temperature);
+                if(errorCode != BMI323_OK){
+                    PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+                    return;
+                }
+                errorCode = bmi323GetSensorTime(bmi323, sensorTime);
+                if(errorCode != BMI323_OK){
+                    PRINT("\033[0;31mFAILED\033[0m; bmi323_ERROR: %02X", errorCode);
+                    return;
+                }
                 PRINT("%04i (%.4fs): %f°C", index, sensorTime, temperature);
 
                 index++;
             }
-        }
-        Catch(exception) {
-            PRINT("ERROR: %i", exception);
-            continue;
-        }
     }
 }
 static void testTemperature(void) {
